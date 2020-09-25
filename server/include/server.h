f936296fbb8ccf64ffd26f953c17624f35ce1942
Illiass <48564017+Illiass@users.noreply.github.com>
2020-09-25 14:38:46 +0300

    main

--- server/include/server.h
+++ server/include/server.h
@@ -1,4 +1,13 @@
 #ifndef UCHAT_TEST_SERVER_H
 #define UCHAT_TEST_SERVER_H
 
-#endif //UCHAT_TEST_SERVER_H
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<pthread.h>
+
+
+#endif
