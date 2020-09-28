a6d9b022553074b71df9b62a5f20d0a2b82a01d3
fightfordobro <fightfordobro@gmail.com>
2020-09-28 19:58:49 +0300

    Test jansson Version 0.1

--- client/src/main.c
+++ client/src/main.c
@@ -1,88 +1,143 @@
 #include <gtk/gtk.h>
+#include <jansson.h>
 
+typedef struct s_user_data
+{
+    char *type;
 
-void create_window(GtkWidget *button, gpointer window) {
+    const gchar *login;
+    const gchar *pass;
 
-    GtkWidget *win, *label;
+    char *options;
+}               t_user_data;
 
-//Username and Password to validate credentials
-    const char *USERNAME = "user";
-    const char *PASSWORD = "123456";
+typedef struct s_login_page
+{
+    GtkWidget *window;
+    GtkWidget *grid;
+    GtkWidget *Login_button;
+    GtkWidget *username;
+    GtkWidget *pass;
+    GtkWidget *label_user;
+    GtkWidget *label_pass;
+}              t_login_page;
 
-
-//Checking if user's entered credentials are equal to actual USERNAME and PASSWORD
-    if (1)
-    {
-        label = gtk_label_new("Username and Password is correct.");
-        win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-        gtk_container_add(GTK_CONTAINER(win), label);
-        gtk_widget_show_all(win);
-    }
-
-    else if (0)
-
-    {
-        label = gtk_label_new("Username and Password is incorrect.");
-        win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-        gtk_container_add(GTK_CONTAINER(win), label);
-        gtk_widget_show_all(win);
-
-    }
-
-}
 
 static void destroy(GtkWidget *widget, gpointer data){
     gtk_main_quit();
 }
 
-int main(int argc, char *argv[]){
+static char *encode(char *pass)
+{
+    printf("Original pass: %s\n", pass);
 
-    GtkWidget *window;
-    GtkWidget *grid;
-    GtkWidget *Login_button, *Quit_button;
-    GtkWidget *u_name;
-    GtkWidget *pass;
-    GtkWidget *label_user;
-    GtkWidget *label_pass;
-    GtkWidget  *button_container;
+    size_t seed = (int)(rand()) % 100000000;
+    size_t pass_len = strlen(pass);
 
-    gtk_init(&argc, &argv);
+    for (size_t i = 0, local_seed = seed; i < pass_len; i++) {
+        pass[i] = pass[i] ^ (local_seed % 1000);
+        local_seed /= 10;
+    }
 
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Login page");
-    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+    printf("Encoded pass: %s\n", pass);
 
-    grid = gtk_grid_new();
-    gtk_grid_set_row_spacing(GTK_GRID(grid), 3);
-    gtk_container_add(GTK_CONTAINER(window), grid);
+    for (size_t i = 0, local_seed = seed; i < pass_len; i++) {
+        pass[i] = pass[i] ^ (local_seed % 1000);
+        local_seed /= 10;
+    }
 
-    label_user = gtk_label_new("Username  ");
-    label_pass = gtk_label_new("Password  ");
+    printf("Decoded pass pass: %s\n", pass);
 
-    u_name = gtk_entry_new();
-    gtk_entry_set_placeholder_text(GTK_ENTRY(u_name), "Username");
-    gtk_grid_attach(GTK_GRID(grid), label_user, 0, 1, 1, 1);
-    gtk_grid_attach(GTK_GRID(grid), u_name, 1, 1, 2, 1);
+    return pass;
+}
 
-    pass = gtk_entry_new();
-    gtk_entry_set_placeholder_text(GTK_ENTRY(pass), "Password");
-    gtk_grid_attach(GTK_GRID(grid), label_pass, 0, 2, 1, 1);
-    gtk_entry_set_visibility(GTK_ENTRY(pass), 0);
-    gtk_grid_attach(GTK_GRID(grid), pass, 1, 2, 1, 1);
+static t_user_data *create_user_data(GtkWidget *button, t_login_page *login_page)
+{
+    t_user_data *userData = malloc(sizeof(t_user_data));
 
-    Login_button = gtk_button_new_with_label("Log in");
-    g_signal_connect(Login_button, "clicked", G_CALLBACK(create_window), NULL);
-    gtk_grid_attach(GTK_GRID(grid), Login_button, 0, 3, 2, 1);
+    json_t *json = json_object();
 
-    Quit_button = gtk_button_new_with_label("Quit");
-    g_signal_connect(Quit_button, "clicked", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_grid_attach(GTK_GRID(grid), Quit_button, 0, 4, 2, 1);
+    json_t *user = json_object();
+    json_object_set_new(user, "nickname", json_string(gtk_entry_get_text((GtkEntry *) login_page->username)));
+    json_object_set_new(user, "password", json_string(gtk_entry_get_text((GtkEntry *) login_page->pass)));
+    json_object_set_new(user, "email", json_string("test"));
+    json_object_set_new(user, "age", json_string("test"));
+    json_object_set_new(user, "fullname", json_string("test"));
+    json_object_set_new(user, "ph_number", json_string("test"));
+    json_object_set_new(user, "user_photo", json_string("test"));
+    json_object_set_new(user, "option", json_string("test"));
 
-    gtk_widget_show_all(window);
 
-    gtk_main();
+    json_object_set_new(json, "type", json_string("log_in"));
+    json_object_set_new(json, "user", user);
 
-    return 0;
+    char *s = json_dumps(json, 0);
+
+    puts(s);
+    json_decref(json);
+
+    return userData;
+}
+
+static void init_login_page(t_login_page **login_page)
+{
+    /* Window Creation */
+    (*login_page)->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW((*login_page)->window), "Login page");
+    gtk_window_set_position(GTK_WINDOW((*login_page)->window), GTK_WIN_POS_CENTER);
+    gtk_container_set_border_width(GTK_CONTAINER((*login_page)->window), 25);
+    gtk_window_set_resizable(GTK_WINDOW((*login_page)->window), FALSE);
+    /*-----------------*/
+
+    /* Grid Creation */
+    (*login_page)->grid = gtk_grid_new();
+    gtk_grid_set_row_spacing(GTK_GRID((*login_page)->grid), 3);
+    gtk_container_add(GTK_CONTAINER((*login_page)->window), (*login_page)->grid);
+    /*--------------*/
+
+    /* Label Creation */
+    (*login_page)->label_user = gtk_label_new("Username  ");
+    (*login_page)->label_pass = gtk_label_new("Password  ");
+    /*----------------*/
+
+    /* Entry Creation */
+
+    /* UserName */
+    (*login_page)->username = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY((*login_page)->username), "Username");
+    gtk_grid_attach(GTK_GRID((*login_page)->grid), (*login_page)->label_user, 0, 1, 1, 1);
+    gtk_grid_attach(GTK_GRID((*login_page)->grid), (*login_page)->username, 1, 1, 2, 1);
+
+    /* Password */
+    (*login_page)->pass = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY((*login_page)->pass), "Password");
+    gtk_grid_attach(GTK_GRID((*login_page)->grid), (*login_page)->label_pass, 0, 2, 1, 1);
+    gtk_entry_set_visibility(GTK_ENTRY((*login_page)->pass), 0);
+    gtk_grid_attach(GTK_GRID((*login_page)->grid), (*login_page)->pass, 1, 2, 1, 1);
+
+    /*----------------*/
+
+    /* Button Creation */
+    (*login_page)->Login_button = gtk_button_new_with_label("Log in");
+    /*----------------*/
+
+}
+
+int main(int argc, char *argv[])
+{
+    char *pass = calloc(sizeof(char), 8);
+    pass = strdup("123321");
+    encode(pass);
+//    gtk_init_check(&argc, &argv);
+//    t_login_page *login_page = malloc(sizeof(t_login_page));
+//    init_login_page(&login_page);
+//
+//    g_signal_connect(login_page->Login_button, "clicked",
+//                     G_CALLBACK(create_user_data), login_page);
+//
+//    gtk_grid_attach(GTK_GRID(login_page->grid), login_page->Login_button, 0, 3, 2, 1);
+//
+//    gtk_widget_show_all(login_page->window);
+//    gtk_main();
+
 }
