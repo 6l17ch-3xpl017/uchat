d2b49f4b34a2d47872b9bf1eac17f049031e4d83
fightfordobro <fightfordobro@gmail.com>
2020-09-29 14:31:57 +0300

    Simple encoding algo

--- client/src/main.c
+++ client/src/main.c
@@ -31,18 +31,18 @@ static char *encode(char *pass)
 {
     printf("Original pass: %s\n", pass);
 
-    size_t seed = (int)(rand()) % 100000000;
+    size_t seed = (int)(rand()) % 100000000 + (int)(rand()) % 100000000;
     size_t pass_len = strlen(pass);
 
     for (size_t i = 0, local_seed = seed; i < pass_len; i++) {
-        pass[i] = pass[i] ^ (local_seed % 1000);
+        pass[i] = pass[i] ^ (((local_seed % 1000) + (local_seed / 1000)));
         local_seed /= 10;
     }
 
     printf("Encoded pass: %s\n", pass);
 
     for (size_t i = 0, local_seed = seed; i < pass_len; i++) {
-        pass[i] = pass[i] ^ (local_seed % 1000);
+        pass[i] = pass[i] ^ (((local_seed % 1000) + (local_seed / 1000)));
         local_seed /= 10;
     }
 
