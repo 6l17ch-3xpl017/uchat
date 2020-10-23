2f91fade76a2b5eff6f9da4a12f1b66714108221
fightfordobro <fightfordobro@gmail.com>
2020-10-23 18:46:14 +0300

    msg send test 2.0

--- server/src/router/auth/send_errors.c
+++ server/src/router/auth/send_errors.c
@@ -5,8 +5,8 @@
 */
 void send_status(int socketfd, int status, char *func) {
     json_t *json = json_object();
-    json_object_set_new(json, "type:", json_string(func));
-    json_object_set_new(json, "status:", json_string(mx_itoa(status)));
+    json_object_set_new(json, "type", json_string(func));
+    json_object_set_new(json, "status", json_integer(status));
     char *result = json_dumps(json, 0);
     write(socketfd, result, strlen(result));
     json_decref(json);
